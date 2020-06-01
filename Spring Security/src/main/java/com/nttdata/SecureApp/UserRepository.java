eb194cbbdf2b82a105a8af0b82daacff2685345c
aishwaryajaiswal <62687294+aishwaryajaiswal@users.noreply.github.com>
2020-06-01 23:11:54 +0530

    Spring Security

---
+++ Spring Security/src/main/java/com/nttdata/SecureApp/UserRepository.java
@@ -0,0 +1,8 @@
+package com.nttdata.SecureApp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUsername(String username);
+}
